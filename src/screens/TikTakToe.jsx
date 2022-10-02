b14a87912257727241806bb8d74add5b65b668db
Emmy britt <beritogwu@gmail.com>
2022-10-02 21:28:49 +0100

    Modified the app.tsx

--- src/screens/TikTakToe.jsx
+++ src/screens/TikTakToe.jsx
@@ -1,65 +1,63 @@
-import React, { useState } from 'react'
-import Board from '../components/Board'
+import React, { useState } from "react";
+import Board from "../components/Board";
 const rowStyle = {
-  display: 'flex'
-}
+  display: "flex",
+};
 
 const squareStyle = {
-  'width': '60px',
-  'height': '60px',
-  'backgroundColor': '#ddd',
-  'margin': '4px',
-  'display': 'flex',
-  'justifyContent': 'center',
-  'alignItems': 'center',
-  'fontSize': '20px',
-  'color': 'white'
-}
+  width: "60px",
+  height: "60px",
+  backgroundColor: "#ddd",
+  margin: "4px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "20px",
+  color: "white",
+};
 
 const boardStyle = {
-  'backgroundColor': '#eee',
-  'width': '208px',
-  'alignItems': 'center',
-  'justifyContent': 'center',
-  'display': 'flex',
-  'flexDirection': 'column',
-  'border': '3px #eee solid'
-}
+  backgroundColor: "#eee",
+  width: "208px",
+  alignItems: "center",
+  justifyContent: "center",
+  display: "flex",
+  flexDirection: "column",
+  border: "3px #eee solid",
+};
 
 const containerStyle = {
-  'display': 'flex',
-  'alignItems': 'center',
-  'flexDirection': 'column'
-}
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+};
 
 const instructionsStyle = {
-  'marginTop': '5px',
-  'marginBottom': '5px',
-  'fontWeight': 'bold',
-  'fontSize': '16px',
-}
+  marginTop: "5px",
+  marginBottom: "5px",
+  fontWeight: "bold",
+  fontSize: "16px",
+};
 
 const buttonStyle = {
-  'marginTop': '15px',
-  'marginBottom': '16px',
-  'width': '80px',
-  'height': '40px',
-  'backgroundColor': '#8acaca',
-  'color': 'white',
-  'fontSize': '16px',
-}
-
-
+  marginTop: "15px",
+  marginBottom: "16px",
+  width: "80px",
+  height: "40px",
+  backgroundColor: "#8acaca",
+  color: "white",
+  fontSize: "16px",
+};
 
 const TikTakToe = () => {
-  const [turn, setTurn] = useState('X');
-  const [cells, setCells] = useState(Array(9).fill(''));
+  const [turn, setTurn] = useState("X");
+  const [cells, setCells] = useState(Array(9).fill(""));
   const [winner, setWinner] = useState();
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   function handleResetGame() {
-    setCells(Array(9).fill(''));
-    setWinner()
+    setCells(Array(9).fill(""));
+    setWinner();
   }
 
   function handleCheckWinner(squares) {
@@ -68,46 +66,52 @@ const TikTakToe = () => {
       across: [
         [0, 1, 2],
         [3, 4, 5],
-        [6, 7, 8]
+        [6, 7, 8],
       ],
       down: [
         [0, 3, 6],
         [1, 4, 7],
-        [2, 5, 8]
+        [2, 5, 8],
       ],
       diagonal: [
         [0, 4, 8],
         [2, 4, 6],
       ],
-    }
+    };
     for (let bi in bis) {
       bis[bi].forEach((value) => {
         // console.log(value)
-        if (squares[value[0]] === '' || squares[value[1]] === '' || squares[value[2]] === '') {
-
-        } else if (squares[value[0]] === squares[value[1]] && squares[value[1]] === squares[value[2]]) {
+        if (
+          squares[value[0]] === "" ||
+          squares[value[1]] === "" ||
+          squares[value[2]] === ""
+        ) {
+        } else if (
+          squares[value[0]] === squares[value[1]] &&
+          squares[value[1]] === squares[value[2]]
+        ) {
           setWinner(squares[value[0]]);
-          setCount(0)
+          setCount(0);
         }
-      })
+      });
     }
   }
 
   function handleClick(num) {
-    setCount(prev => prev + 1);
+    setCount((prev) => prev + 1);
     console.log(count);
     if (winner) return;
-    if (cells[num] !=='') return alert('Already clicked');
+    if (cells[num] !== "") return alert("Already clicked");
     let squares = [...cells];
-    if (turn === 'X') {
-      squares[num] = 'X'
-      setTurn('O');
+    if (turn === "X") {
+      squares[num] = "X";
+      setTurn("O");
     } else {
-      setTurn('X');
-      squares[num] = 'O'
+      setTurn("X");
+      squares[num] = "O";
     }
-    if (count >8 && !winner) {
-      alert('Game over no winner for today.')
+    if (count > 8 && !winner) {
+      alert("Game over no winner for today.");
     }
 
     handleCheckWinner(squares);
@@ -116,10 +120,16 @@ const TikTakToe = () => {
   return (
     <div className="game">
       <div className="game-board">
-        <Board handleResetGame={handleResetGame} winner={winner} cells={cells} turn={turn} handleClick={handleClick} />
+        <Board
+          handleResetGame={handleResetGame}
+          winner={winner}
+          cells={cells}
+          turn={turn}
+          handleClick={handleClick}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TikTakToe
+export default TikTakToe;
